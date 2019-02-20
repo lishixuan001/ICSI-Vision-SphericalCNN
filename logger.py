535d4c59c214126945acf2e3094767ba7ed0cb57
Leiay <yllucy@yeah.net>
2019-02-19 16:32:33 -0800

    automatically mkdir log

--- logger.py
+++ logger.py
@@ -1,12 +1,15 @@
 import logging
 import datetime
 import sys
+import os
 
 
 def setup_logger(name):
     now = datetime.datetime.now().strftime('%m%d%H%M%S')
     formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(message)s',
                                   datefmt='%Y-%m-%d %H:%M:%S')
+    if not os.path.exists("./log"):
+        os.mkdir("./log")
     handler = logging.FileHandler('log/{}.log'.format(now), mode='w')
     handler.setFormatter(formatter)
     screen_handler = logging.StreamHandler(stream=sys.stdout)
